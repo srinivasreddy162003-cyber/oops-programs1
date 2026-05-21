66976e517756327046c70fc97f4cab4c09c84e9f
chkir <chkir@LAPTOP-D3PF2C6A>
2026-05-21 12:21:45 +0530

    Add project files.

---
+++ oops-programs1/Program.cs
@@ -0,0 +1,32 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace oops_programs1
+//{
+//    class newclass
+//    {
+//        int a;
+//        int b;
+//        public newclass(int a, int b)
+//        {
+//            this.a = a;
+//            this.b = b;
+//        }
+//        public void add()
+//        {
+//            int c=a+b;
+//            Console.WriteLine("sum is "+c);
+//        }
+//    }
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            newclass obj = new newclass(10, 20);
+//            obj.add();  
+//        }
+//    }
+//}
---
+++ oops-programs1/Properties/constructs.git.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oops_programs1.Properties
+{
+    class newclass1
+    {
+        int sid;
+        string sname;
+        static string sclg = "vignan university";
+    public newclass1(int sid, string sname)
+        {
+            this.sid = sid;
+            this.sname = sname;
+        }
+        public void stddetails()
+        {
+            Console.WriteLine("student id is " + sid);
+            Console.WriteLine("student name is " + sname);
+            Console.WriteLine("student college name is " + sclg);
+            Console.WriteLine("\n");
+        }
+    }
+    internal class constructs
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter number of students ");
+            int N = int.Parse(Console.ReadLine());
+
+            newclass1[] std = new newclass1[N];
+            for(int i= 0; i < N; i++)
+            {
+                Console.WriteLine("\n Enter students details");
+
+                Console.WriteLine("enter student id");
+                int id = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("enter student name");
+                string name= Console.ReadLine();
+
+                std[i] = new newclass1(id, name);
+
+            }
+            Console.WriteLine("student details are");
+            for(int i=0; i < std.Length; i++)
+            {
+                std[i].stddetails();
+            }
+
+        }
+    }
+}
